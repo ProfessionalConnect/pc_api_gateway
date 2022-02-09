2411ad1e1c9d929784c5d772c1163ba8716df3c2
koreaaerospaceU <zx811@naver.com>
2022-02-10 02:11:36 +0900

    feat: Authorization Http Header 오브젝트 정의

---
+++ src/main/kotlin/com/pro/gateway/security/SecurityHeader.kt
@@ -0,0 +1,10 @@
+package com.pro.gateway.security
+
+/**
+ * Created by Minky on 2022-02-10
+ */
+
+object SecurityHeader {
+    const val UUID = "uuid"
+    const val X_AUTH_TOKEN = "X-AUTH-TOKEN"
+}
